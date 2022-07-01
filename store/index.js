f97ddf29c743879d194fc91109e02d1d38d8bb31
Nieb Anupat <anupat.k64@rsu.ac.th>
2022-07-01 16:35:20 +0700

    make it better

--- store/index.js
+++ store/index.js
@@ -3,41 +3,36 @@ export const state = () => ({})
 export const getters = {}
 
 export const mutations = {
-      ON_AUTH_STATE_CHANGED_MUTATION(state){
-
-      }
+  ON_AUTH_STATE_CHANGED_MUTATION(state) {},
 }
 
 export const actions = {
+  nuxtServerInit({ commit, dispatch }, { req }) {
+    // console.log('nuxtServerInit....')
 
-      nuxtServerInit({ commit, dispatch}, { req }) {
-            // console.log('nuxtServerInit....')
-        
-            // if (process.server && process.static) return;
-            // if (!req.headers.cookie) return;
-        
-            // const parsed = CookieParser.parse(req.headers.cookie)
-            // const access_token = parsed.access_token
-        
-            // if (!access_token) return;
-        
-            // const decoded = JWTDecode(access_token)
-        
-            // if (!decoded) return;
-         
-            // const user = {
-            //   uid: decoded.user_id,
-            //   email: decoded.email,
-            // }
-            
-        
-            // await commit('users/setUser', user)
-            // await dispatch('todos/fetchTodos');
-          
-            console.log('user logged in')
-            commit('todos/setTodos',{title:'Test01',status:false})
-          },
-      onAuthStateChangedAction(state){
-            
-      }
+    // if (process.server && process.static) return;
+    // if (!req.headers.cookie) return;
+
+    // const parsed = CookieParser.parse(req.headers.cookie)
+    // const access_token = parsed.access_token
+
+    // if (!access_token) return;
+
+    // const decoded = JWTDecode(access_token)
+
+    // if (!decoded) return;
+
+    // const user = {
+    //   uid: decoded.user_id,
+    //   email: decoded.email,
+    // }
+
+    // await commit('users/setUser', user)
+    // await dispatch('todos/fetchTodos');
+
+    await dispatch('todos/add', { title: 'Test01', status: false }).then(() => {
+      console.log('add task success')
+    })
+  },
+  onAuthStateChangedAction(state) {},
 }
