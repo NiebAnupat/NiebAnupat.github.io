4396dcd9b106ff184bcc5edaaf32b3656bd1420e
Nieb Anupat <anupat.k64@rsu.ac.th>
2022-07-01 16:23:35 +0700

    make it better

--- store/index.js
+++ store/index.js
@@ -9,6 +9,33 @@ export const mutations = {
 }
 
 export const actions = {
+
+      nuxtServerInit({ commit, dispatch}, { req }) {
+            // console.log('nuxtServerInit....')
+        
+            // if (process.server && process.static) return;
+            // if (!req.headers.cookie) return;
+        
+            // const parsed = CookieParser.parse(req.headers.cookie)
+            // const access_token = parsed.access_token
+        
+            // if (!access_token) return;
+        
+            // const decoded = JWTDecode(access_token)
+        
+            // if (!decoded) return;
+         
+            // const user = {
+            //   uid: decoded.user_id,
+            //   email: decoded.email,
+            // }
+            
+        
+            // await commit('users/setUser', user)
+            // await dispatch('todos/fetchTodos');
+          
+            console.log('user logged in')
+          },
       onAuthStateChangedAction(state){
             
       }
